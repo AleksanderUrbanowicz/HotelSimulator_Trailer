348cdf3bbe4877e8d7a6e426c026b505ffa648dc
AleksanderUrbanowicz <57544016+AleksanderUrbanowicz@users.noreply.github.com>
2020-02-19 14:46:12 +0100

    before renders shooting

---
+++ Assets/_Prefabs/AssetsToPick/RendersTempScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RendersTempScript : MonoBehaviour
+{
+    public List<Transform> items;
+    private int index = 0;
+    void Start()
+    {
+        if (items != null)
+        {
+            foreach (Transform t in items)
+            {
+                t.gameObject.SetActive(false);
+
+            }
+                }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if( Input.GetKeyDown(KeyCode.N))
+            {
+            Next();
+        }
+    }
+
+    void Next()
+    {
+        items[index].gameObject.SetActive(false);
+        index = (index + 1) % items.Count;
+        items[index].gameObject.SetActive(true);
+
+    }
+}
