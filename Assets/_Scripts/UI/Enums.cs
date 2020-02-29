6473c4ce69c68cea49882781fbdd576ec3b10e30
AleksanderUrbanowicz <57544016+AleksanderUrbanowicz@users.noreply.github.com>
2020-03-01 00:19:52 +0100

    PlugginableImage, PlugginableText

---
+++ Assets/_Scripts/UI/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIColorType
+{
+    BACKGROUND_MAIN,
+    BACKGROUND_LIGHT,
+    BACKGROUND_DARK,
+    FONT_LIGHT,
+    FONT_DARK
+
+}
---
+++ Assets/_Scripts/UI/PlugginableText.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Text))]
+    [ExecuteInEditMode]
+    public class PlugginableText : PluggableUIElement
+    {
+
+        public Text text;
+        public int color;
+        private UIColorType uIColorType;
+        public Text Text { get => text != null ? text : GetComponent<Text>(); }
+        public ThemeData ThemeData { get => themeData != null ? themeData : Resources.Load<ThemeData>(""); }
+        public override void Awake()
+        {
+            
+            base.Awake();
+
+        }
+        public override void Update()
+        {
+           
+            base.Update();
+
+        }
+
+
+
+        protected override void OnThemeDraw()
+        {
+            SetColor();
+            base.OnThemeDraw();
+
+
+        }
+
+        public void SetColor()
+        {
+            Text.color = ThemeData != null ? ThemeData.GetColor(color) : Color.magenta;
+
+        }
+
+
+    }
+}
